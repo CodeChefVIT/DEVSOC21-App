43221630e7c9f9f3c6257b70bbd21a9ac7a633ab
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-03-31 19:48:45 +0530

    web view

--- devsoc_app/lib/screens/funScreen.dart
+++ devsoc_app/lib/screens/funScreen.dart
@@ -1,6 +1,7 @@
 import 'package:devsoc_app/helpers/size.dart';
 import 'package:devsoc_app/helpers/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Fun extends StatefulWidget {
   @override
@@ -12,22 +13,55 @@ class _FunState extends State<Fun> {
   @override
   Widget build(BuildContext context) {
     SizeHelper s = SizeHelper(context);
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: s.wHelper(6),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: s.hHelper(8),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: s.wHelper(6),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: s.hHelper(15),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  color: t.activeColor,
+                ),
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  width: s.wHelper(100),
+                  height: s.hHelper(60),
+                  child: WebView(
+                    javascriptMode: JavascriptMode.unrestricted,
+                    initialUrl: 'https://hextris.io',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            color: t.bgColor,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: s.hHelper(8),
+                ),
+                Text(
+                  "Fun",
+                  style: t.heading,
+                ),
+              ],
             ),
-            Text(
-              "Fun",
-              style: t.heading,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
