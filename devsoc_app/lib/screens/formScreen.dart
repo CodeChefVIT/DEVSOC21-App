c0c37451b549ef3472d660fe7e975aed3d4c0bd0
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-04-03 22:27:18 +0530

    testing form

--- devsoc_app/lib/screens/formScreen.dart
+++ devsoc_app/lib/screens/formScreen.dart
@@ -8,26 +8,79 @@ class Forms extends StatefulWidget {
 }
 
 class _FormsState extends State<Forms> {
+  Map<String, dynamic> form = {
+    "title": "Pizza Form",
+    "questions": [
+      {
+        "question": "Email",
+        "type": "textfield",
+        "value": null,
+      },
+      {
+        "question": "Name",
+        "type": "textfield",
+        "value": null,
+      },
+      {
+        "question": "T Shirt Size",
+        "type": "dropdown",
+        "dropdownOptions": [
+          "S",
+          "M",
+          "L",
+          "XL",
+        ],
+        "value": null,
+      },
+      {
+        "question": "Tech Stack",
+        "type": "checkbox",
+        "checkboxOptions": [
+          "Frontend Web",
+          "App",
+          "Backend",
+          "Design",
+        ],
+        "value": null,
+      },
+    ],
+  };
   ThemeHelper t = ThemeHelper();
   @override
   Widget build(BuildContext context) {
     SizeHelper s = SizeHelper(context);
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: s.wHelper(6),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: s.hHelper(8),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: s.wHelper(6),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: s.hHelper(14),
+              ),
+            ],
+          ),
+          Container(
+            color: t.bgColor,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: s.hHelper(8),
+                ),
+                Text(
+                  "Forms",
+                  style: t.heading,
+                ),
+              ],
             ),
-            Text(
-              "Forms",
-              style: t.heading,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
