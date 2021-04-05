213cf2119d9fa82d36355c34555880e17e5347c2
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-04-06 01:42:34 +0530

    ptr tm

--- devsoc_app/lib/api/timeline.dart
+++ devsoc_app/lib/api/timeline.dart
@@ -2,19 +2,6 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/state_manager.dart';
 
 class TimelineChecker extends GetxController {
-  // List<DateTime> timelineNodes = [
-  //   DateTime.parse("2021-03-29 18:00:00").toLocal(),
-  //   DateTime.parse("2021-03-29 20:30:00").toLocal(),
-  //   DateTime.parse("2021-03-29 23:30:00").toLocal(),
-  //   DateTime.parse("2021-03-30 02:00:00").toLocal(),
-  //   DateTime.parse("2021-03-30 06:30:00").toLocal(),
-  //   DateTime.parse("2021-03-30 14:30:00").toLocal(),
-  //   DateTime.parse("2021-03-30 18:30:00").toLocal(),
-  //   DateTime.parse("2021-03-31 02:00:00").toLocal(),
-  //   DateTime.parse("2021-03-31 06:30:00").toLocal(),
-  //   DateTime.parse("2021-03-31 14:30:00").toLocal(),
-  // ];
-
   Map<String, dynamic> timeline = {
     "today": 2,
     "day1": [
@@ -134,19 +121,6 @@ class TimelineChecker extends GetxController {
 
   List<RxDouble> percentNodeOne = [];
 
-  // List<RxBool> timelineBool = [
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  //   false.obs,
-  // ];
-  //
   RxInt current = 0.obs;
 
   void checkDay() {
