ecbd96e6aa5f4d368d01ccb2cfa558080a481748
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-04-11 17:47:25 +0530

    cached profile page and fixed font weights

--- devsoc_app/lib/api/profile.dart
+++ devsoc_app/lib/api/profile.dart
@@ -4,18 +4,46 @@ import 'package:devsoc_app/constants/links.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/state_manager.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileGet extends GetxController {
   RxBool profileLoader = true.obs;
 
   Future<Map> getProfile(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey("profile")) {
+      var url = Uri.parse(profileRoute);
+      try {
+        var response = await http.get(url, headers: {
+          HttpHeaders.authorizationHeader: token,
+        });
+        var res = json.decode(response.body);
+        print(res);
+        prefs.setString("profile", response.body);
+        profileLoader.value = false;
+        return res;
+      } catch (e) {
+        return {"message": "error"};
+      }
+    } else {
+      profileLoader.value = true;
+      var res = json.decode(prefs.getString("profile"));
+      profileLoader.value = false;
+      return res;
+    }
+  }
+
+  Future<Map> refreshProfile(String token) async {
+    final prefs = await SharedPreferences.getInstance();
     var url = Uri.parse(profileRoute);
     try {
+      profileLoader.value = true;
       var response = await http.get(url, headers: {
         HttpHeaders.authorizationHeader: token,
       });
       var res = json.decode(response.body);
       print(res);
+      prefs.setString("profile", response.body);
       profileLoader.value = false;
       return res;
     } catch (e) {
--- devsoc_app/lib/screens/profileScreen.dart
+++ devsoc_app/lib/screens/profileScreen.dart
@@ -12,6 +12,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -23,12 +24,23 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   ThemeHelper t = ThemeHelper();
   String token;
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
   ProfileGet profile = ProfileGet();
   Map profileDetails = {};
   Auth auth = Auth();
   void initState() {
     getData();
     super.initState();
+  }
+
+  void _onRefresh() async {
+    final prefs = await SharedPreferences.getInstance();
+    final extractedUserData =
+        json.decode(prefs.getString('userData')) as Map<String, Object>;
+    token = extractedUserData['token'];
+    profileDetails = await profile.refreshProfile(token);
+    _refreshController.refreshCompleted();
   }
 
   getData() async {
@@ -58,177 +70,198 @@ class _ProfileState extends State<Profile> {
               )
             : Stack(
                 children: [
-                  SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: s.hHelper(16),
-                        ),
-                        Container(
-                          height: s.hHelper(16),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: t.secondaryBgColor,
+                  SmartRefresher(
+                    enablePullDown: true,
+                    header: CustomHeader(
+                      builder: (BuildContext context, _) {
+                        return Center(
+                          child: CupertinoActivityIndicator(),
+                        );
+                      },
+                    ),
+                    controller: _refreshController,
+                    onRefresh: _onRefresh,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: s.hHelper(16),
                           ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: Image.network(
-                              profileDetails["user"]["avatar"],
-                              fit: BoxFit.contain,
+                          Container(
+                            height: s.hHelper(16),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: t.secondaryBgColor,
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                profileDetails["user"]["avatar"],
+                                fit: BoxFit.contain,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: s.hHelper(2),
-                        ),
-                        Text(
-                          profileDetails["user"]["name"],
-                          style: t.subheading,
-                        ),
-                        SizedBox(
-                          height: s.hHelper(1),
-                        ),
-                        profileDetails["user"]["team"] == null
-                            ? TextButton(
-                                onPressed: () {
-                                  launch(devsocLink);
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: t.activeColor,
-                                  ),
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: s.wHelper(5),
-                                    vertical: s.hHelper(1),
-                                  ),
-                                  child: Text(
-                                    "Create a team",
-                                    style: t.smallText,
-                                  ),
-                                ),
-                              )
-                            : Text(
-                                "Team " +
-                                    profileDetails["user"]["team"]["name"],
-                                style: t.smallTextBold,
-                              ),
-                        SizedBox(
-                          height: s.hHelper(3),
-                        ),
-                        profileDetails["user"]["team"] == null
-                            ? Text(
-                                "Create a team to submit an Idea",
-                                style: t.smallText,
-                              )
-                            : profileDetails["user"]["team"]["submission"] ==
-                                    null
-                                ? TextButton(
-                                    onPressed: () {
-                                      launch(devsocLink);
-                                    },
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: t.activeColor,
-                                      ),
-                                      padding: EdgeInsets.symmetric(
-                                        horizontal: s.wHelper(5),
-                                        vertical: s.hHelper(1),
-                                      ),
-                                      child: Text(
-                                        "Submit an Idea",
-                                        style: t.smallText,
-                                      ),
+                          SizedBox(
+                            height: s.hHelper(2),
+                          ),
+                          Text(
+                            profileDetails["user"]["name"],
+                            style: t.subheading,
+                          ),
+                          SizedBox(
+                            height: s.hHelper(1),
+                          ),
+                          profileDetails["user"]["team"] == null
+                              ? TextButton(
+                                  onPressed: () {
+                                    launch(devsocLink);
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: t.activeColor,
                                     ),
-                                  )
-                                : Column(
-                                    children: [
-                                      Text(
-                                        "Idea " +
-                                            profileDetails["user"]["team"]
-                                                ["submission"]["name"],
-                                        style: t.smallTextBold,
-                                      ),
-                                      SizedBox(
-                                        height: s.hHelper(0.5),
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            profileDetails["user"]["team"]
-                                                ["submission"]["status"],
-                                            style: t.smallText,
-                                          ),
-                                          SizedBox(
-                                            width: s.wHelper(2),
-                                          ),
-                                          Icon(
-                                            CupertinoIcons.check_mark,
-                                            color: t.acceptColor,
-                                            size: 22,
-                                          ),
-                                        ],
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: s.wHelper(5),
+                                      vertical: s.hHelper(1),
+                                    ),
+                                    child: Text(
+                                      "Create a team",
+                                      style: t.smallText,
+                                    ),
+                                  ),
+                                )
+                              : RichText(
+                                  text: TextSpan(
+                                    text: 'Team ',
+                                    style: t.smallTextBold,
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                        text: profileDetails["user"]["team"]
+                                            ["name"],
+                                        style: t.smallText,
                                       ),
                                     ],
                                   ),
-                        SizedBox(
-                          height: s.hHelper(3),
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: t.altBgColor,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
+                                ),
+                          SizedBox(
+                            height: s.hHelper(3),
                           ),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: s.wHelper(5),
-                            vertical: s.hHelper(2),
+                          profileDetails["user"]["team"] == null
+                              ? Text(
+                                  "Create a team to submit an Idea",
+                                  style: t.smallText,
+                                )
+                              : profileDetails["user"]["team"]["submission"] ==
+                                      null
+                                  ? TextButton(
+                                      onPressed: () {
+                                        launch(devsocLink);
+                                      },
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: t.activeColor,
+                                        ),
+                                        padding: EdgeInsets.symmetric(
+                                          horizontal: s.wHelper(5),
+                                          vertical: s.hHelper(1),
+                                        ),
+                                        child: Text(
+                                          "Submit an Idea",
+                                          style: t.smallText,
+                                        ),
+                                      ),
+                                    )
+                                  : Column(
+                                      children: [
+                                        Text(
+                                          "Idea " +
+                                              profileDetails["user"]["team"]
+                                                  ["submission"]["name"],
+                                          style: t.smallTextBold,
+                                        ),
+                                        SizedBox(
+                                          height: s.hHelper(0.5),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              profileDetails["user"]["team"]
+                                                  ["submission"]["status"],
+                                              style: t.smallText,
+                                            ),
+                                            SizedBox(
+                                              width: s.wHelper(2),
+                                            ),
+                                            Icon(
+                                              CupertinoIcons.check_mark,
+                                              color: t.acceptColor,
+                                              size: 22,
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                          SizedBox(
+                            height: s.hHelper(3),
                           ),
-                          width: s.wHelper(90),
-                          child: Text(
-                            profileDetails["user"]["bio"],
-                            style: t.smallText,
-                          ),
-                        ),
-                        SizedBox(
-                          height: s.hHelper(3),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                _launchURL(profileDetails["user"]["personal"]
-                                    ["github"]);
-                              },
-                              child: SvgPicture.asset(
-                                github,
-                                height: s.hHelper(3),
-                                color: t.activeColor,
+                          Container(
+                            decoration: BoxDecoration(
+                              color: t.altBgColor,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
                               ),
                             ),
-                            SizedBox(
-                              width: s.wHelper(10),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: s.wHelper(5),
+                              vertical: s.hHelper(2),
                             ),
-                            GestureDetector(
-                              onTap: () {
-                                _launchURL(profileDetails["user"]["personal"]
-                                    ["linkedin"]);
-                              },
-                              child: SvgPicture.asset(
-                                linkedin,
-                                height: s.hHelper(3),
-                                color: t.activeColor,
+                            width: s.wHelper(90),
+                            child: Text(
+                              profileDetails["user"]["bio"],
+                              style: t.smallText,
+                            ),
+                          ),
+                          SizedBox(
+                            height: s.hHelper(3),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  _launchURL(profileDetails["user"]["personal"]
+                                      ["github"]);
+                                },
+                                child: SvgPicture.asset(
+                                  github,
+                                  height: s.hHelper(3),
+                                  color: t.activeColor,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                              SizedBox(
+                                width: s.wHelper(10),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  _launchURL(profileDetails["user"]["personal"]
+                                      ["linkedin"]);
+                                },
+                                child: SvgPicture.asset(
+                                  linkedin,
+                                  height: s.hHelper(3),
+                                  color: t.activeColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   Container(
